a80fc9181951823ce45a76e5307f32b1a34d8ab6
monicaacha2103 <monicaacha2103@gmail.com>
2019-03-17 20:51:27 +0530

    front end added

---
+++ client/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import ContactForm from './components/ContactForm'
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.render(<ContactForm/>, document.getElementById('app'));
---
+++ client/src/components/ContactForm.js
@@ -0,0 +1,53 @@
+import React,{Component} from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+class ContactForm extends Component {
+    constructor (props){
+      super(props);
+      this.state={
+          name:"",
+          email:"",
+          message:""
+      }
+    }
+      handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value})
+      }
+
+      handleSubmit = e => {
+        e.preventDefault();
+        const {name, email, message} = this.state;
+        const form = await axios.post('/api/form',{
+            name,
+            email, 
+            message
+        }) 
+
+      }
+    
+    
+      render(){
+        return(
+            <Form className="contact" onSubmit={this.handleSubmit}>
+            <h1>Contact Form</h1>
+            <FormGroup>
+            <Label for="exampleEmail">Name</Label>
+            <Input type="text" name="name"  placeholder="" onChange={this.handleChange}/>
+          </FormGroup>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input type="email" name="email" placeholder="" onChange={this.handleChange} />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="exampleText">Message</Label>
+              <Input type="textarea" name="message" onChange={this.handleChange} />
+            </FormGroup>
+  
+
+            <Button>Submit</Button>
+          </Form>);
+      }
+    }  
+    
+    export default ContactForm;
